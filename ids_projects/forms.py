9ee3d72aaf5465dbcb5e861c7c82fb345d016167
Andrew <andrewbmagill@yahoo.com>
2016-02-17 00:26:06 -0600

    removing the rest of the data / dataset stuff from projects app

--- ids_projects/forms.py
+++ ids_projects/forms.py
@@ -9,29 +9,3 @@ class ProjectForm(forms.Form):
     )
     investigation_type = forms.ChoiceField(choices=INV_TYPES, required=True)
     description = forms.CharField(max_length=255, widget=forms.Textarea, required=False)
-
-class DatasetForm(forms.Form):
-    SEQUENCING = 'Sequencing'
-    ALIGNMENT = 'Alignment'
-    ANALYSIS = 'Anlysis'
-    VAR_DETECT = 'Variant Detection'
-    VAR_CALLING = 'Variant Calling'
-    ANNOTATION = 'Annotation'
-    GWAS = 'GWAS'
-    OTHER = 'Other'
-    PROC_TYPES = (
-        ('', 'Choose One'),
-        (SEQUENCING,SEQUENCING),
-        (ALIGNMENT,ALIGNMENT),
-        (ANALYSIS, ANALYSIS),
-        (VAR_DETECT, VAR_DETECT),
-        (VAR_CALLING, VAR_CALLING),
-        (ANNOTATION, ANNOTATION),
-        (GWAS, GWAS),
-        (OTHER, OTHER),
-    )
-    process_type = forms.ChoiceField(choices=PROC_TYPES, required=True)
-    seq_method = forms.CharField(max_length=255)
-    seq_hardware = forms.CharField(max_length=255)
-    assem_method = forms.CharField(max_length=255)
-    ref_sequence = forms.CharField(max_length=255)
