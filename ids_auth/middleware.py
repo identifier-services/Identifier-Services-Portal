43fd823ef6919ee6f23f351eeadef5e276a8f3df
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-22 12:54:02 -0600

    logout on refresh error

--- ids_auth/middleware.py
+++ ids_auth/middleware.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from django.contrib.auth import logout
 from agavepy.agave import Agave
 import logging
 import time
@@ -22,20 +22,26 @@ class AgaveTokenRefreshMiddleware(object):
                         len(token['access_token']), '-')
                     logger.debug('refreshing current token: %s' % masked_token)
 
-                    ag = Agave(
-                        api_server=getattr(settings, 'AGAVE_TENANT_BASEURL'),
-                        api_key=getattr(settings, 'AGAVE_CLIENT_KEY'),
-                        api_secret=getattr(settings, 'AGAVE_CLIENT_SECRET'),
-                        token=token['access_token'], refresh_token=token['refresh_token'])
-                    ag.token.refresh()
+                    try:
+                        ag = Agave(
+                            api_server=getattr(settings, 'AGAVE_TENANT_BASEURL'),
+                            api_key=getattr(settings, 'AGAVE_CLIENT_KEY'),
+                            api_secret=getattr(settings, 'AGAVE_CLIENT_SECRET'),
+                            token=token['access_token'], refresh_token=token['refresh_token'])
+                        ag.token.refresh()
 
-                    ag.token.token_info['created'] = current_time
-                    request.session[token_key] = ag.token.token_info
-                    request.session.save()
+                        ag.token.token_info['created'] = current_time
+                        request.session[token_key] = ag.token.token_info
+                        request.session.save()
 
-                    masked_token = ag.token.token_info['access_token'][:8].ljust(
-                        len(ag.token.token_info['access_token']), '-')
-                    logger.debug('refreshed token: %s' % masked_token)
+                        masked_token = ag.token.token_info['access_token'][:8].ljust(
+                            len(ag.token.token_info['access_token']), '-')
+                        logger.debug('refreshed token: %s' % masked_token)
+                    except:
+                        logger.exception('Failed to refresh token for user=%s. '
+                                         'Forcing logout.' % request.user.username)
+                        logout(request)
+
                 else:
                     masked_token = token['access_token'][:8].ljust(
                         len(token['access_token']), '-')
