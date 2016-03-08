b8871e4206b7f2cccd0be0180480e69e1c9b19bf
Andrew <andrewbmagill@yahoo.com>
2016-03-08 15:02:55 -0600

    moving view route to bottom so it matches, i'm sure there's a better way to do this.

--- ids_projects/urls.py
+++ ids_projects/urls.py
@@ -9,12 +9,12 @@ urlpatterns = patterns(
     url(r'^project/(?P<project_id>.+)/specimens/?$', 'list', name='list'),
     # create a specimen related to a project
     url(r'^project/(?P<project_id>.+)/specimen/?$', 'create', name='create'),
-    # view a specimen
-    url(r'^specimen/(?P<specimen_id>.+)/?$', 'view', name='view'),
     # edit a specimen
     url(r'^specimen/(?P<specimen_id>.+)/edit$', 'edit', name='edit'), # or specimen_edit/...?
     # delete a specimen
     url(r'^specimen/(?P<specimen_id>.+)/delete$', 'delete', name='delete'), # or specimen_delete/...?
+    # view a specimen
+    url(r'^specimen/(?P<specimen_id>.+)/?$', 'view', name='view'),
 )
 
 #############
@@ -26,12 +26,12 @@ urlpatterns += patterns(
     url(r'^specimen/(?P<specimen_id>.+)/processes/?$', 'list', name='list'),
     # create a processes related to a specimen
     url(r'^specimen/(?P<specimen_id>.+)/process/?$', 'create', name='create'),
-    # view a process
-    url(r'^process/(?P<process_id>.+)/?$', 'view', name='view'),
     # edit a process
     url(r'^process/(?P<process_id>.+)/edit$', 'edit', name='edit'),
     # delete a process
     url(r'^process/(?P<process_id>.+)/delete$', 'delete', name='delete'),
+    # view a process
+    url(r'^process/(?P<process_id>.+)/?$', 'view', name='view'),
 )
 
 #########
@@ -43,12 +43,12 @@ urlpatterns += patterns(
     url(r'^process/(?P<process_id>.+)/files/?$', 'list', name='list'),
     # create a file metadat object associated with a process
     url(r'^process/(?P<process_id>.+)/file/?$', 'create', name='create'),
-    # view a file metadata object
-    url(r'^file/(?P<data_id>.+)/?$', 'view', name='view'),
     # edit a file metadata object
     url(r'^file/(?P<data_id>.+)/edit$', 'edit', name='edit'),
     # edit a file metadata object
     url(r'^file/(?P<data_id>.+)/delete$', 'delete', name='delete'),
+    # view a file metadata object
+    url(r'^file/(?P<data_id>.+)/?$', 'view', name='view'),
 )
 
 ###########
@@ -70,12 +70,12 @@ urlpatterns += patterns(
     url(r'^project/(?P<project_id>.+)/workflows/?$', 'list', name='list'),
     # create a worflow associated with a project
     url(r'^project/(?P<project_id>.+)/workflow/?$', 'create', name='create'),
-    # view a workflow
-    url(r'^workflow/(?P<workflow_id>.+)/?$', 'view', name='view'),
     # edit a workflow
     url(r'^workflow/(?P<workflow_id>.+)/edit$', 'edit', name='edit'),
     # delete a workflow
     url(r'^workflow/(?P<workflow_id>.+)/delete$', 'delete', name='delete'),
+    # view a workflow
+    url(r'^workflow/(?P<workflow_id>.+)/?$', 'view', name='view'),
 )
 
 ############
@@ -87,10 +87,10 @@ urlpatterns += patterns(
     url(r'^projects/?$', 'list', name='list'),
     # create project
     url(r'^project/?$', 'create', name='create'),
-    # view a specific project
-    url(r'^project/(?P<project_id>.+)/?$', 'view', name='view'),
     # edit project
     url(r'^project/(?P<project_id>.+)/edit$', 'edit', name='edit'), # or project_edit/... ?
     # delete project
     url(r'^project/(?P<project_id>.+)/delete$', 'delete', name='delete'), # or project_delete/... ?
+    # view a specific project
+    url(r'^project/(?P<project_id>.+)/?$', 'view', name='view'),
 )
