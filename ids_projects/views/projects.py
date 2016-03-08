6888d28c860a2a15a137fbf90a41e3e810f7190b
Andrew <andrewbmagill@yahoo.com>
2016-03-08 14:30:05 -0600

    create and edit projects

--- ids_projects/views/projects.py
+++ ids_projects/views/projects.py
@@ -2,12 +2,16 @@ from agavepy.agave import Agave, AgaveException
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import (HttpResponse,
+                         HttpResponseRedirect,
                          HttpResponseBadRequest,
                          HttpResponseForbidden,
                          HttpResponseNotFound)
 from django.shortcuts import render
 import json, logging
-#from forms import ProjectForm
+from ..forms.projects import ProjectForm
+
+
+logger = logging.getLogger(__name__)
 
 
 def _client(request):
@@ -26,6 +30,8 @@ def list(request):
         a = _client(request)
         query = {'name':'idsvc.project'}
         project_list = a.meta.listMetadata(q=json.dumps(query))
+
+        print '\n\n *** list : {} *** \n\n'.format(project_list)
 
         return render(request, 'ids_projects/index.html', {'data':project_list})
 
@@ -47,13 +53,14 @@ def view(request, project_id):
         query = {'uuid':project_id}
         project_list = a.meta.listMetadata(q=json.dumps(query))
 
-        print project_list
+        print '\n\n *** view : {} *** \n\n'.format(project_list)
 
         try:
             project = project_list[0]
         except:
             return HttpResponseNotFound("Project not found")
         else:
+
             return HttpResponse(json.dumps(project), content_type="application/json", status=200)
 
     #########
@@ -69,25 +76,44 @@ def create(request):
     # GET #
     #######
     if request.method == 'GET':
+        # import pdb; pdb.set_trace()
 
-        a = _client(request)
-        query = {'uuid':project_id}
-        project_list = a.meta.listMetadata(q=json.dumps(query))
-
-        print project_list
-
-        try:
-            project = project_list[0]
-        except:
-            return HttpResponseNotFound("Project not found")
-        else:
-            return HttpResponse(json.dumps(project), content_type="application/json", status=200)
+        context = {'form': ProjectForm()}
+        return render(request, 'ids_projects/create.html', context)
 
     ########
     # POST #
     ########
     elif request.method == 'POST':
-        return HttpResponse("Creating new project: {}".format(len(projects)+1))
+
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+
+            user_full = request.user.get_full_name()
+
+            title = form.cleaned_data['title']
+            inv_type = form.cleaned_data['investigation_type']
+            desc = form.cleaned_data['description']
+
+            new_project = {
+                'name' : 'idsvc.project',
+                'associationIds' : [],
+                'value' : {
+                    'title' : title,
+                    'creator' : user_full,
+                    'investigation_type' : inv_type,
+                    'description' : desc,
+                },
+            }
+
+            a = _client(request)
+            try:
+                response = a.meta.addMetadata(body=new_project)
+            except Exception as e:
+                logger.debug('Error while attempting to create project metadata: %s' % e)
+
+        return HttpResponseRedirect('/projects/')
 
     #########
     # OTHER #
@@ -107,20 +133,49 @@ def edit(request, project_id):
         query = {'uuid':project_id}
         project_list = a.meta.listMetadata(q=json.dumps(query))
 
-        print project_list
+        print '\n\n *** edit : {} *** \n\n'.format(project_list)
 
         try:
             project = project_list[0]
         except:
             return HttpResponseNotFound("Project not found")
         else:
-            return HttpResponse(json.dumps(project), content_type="application/json", status=200)
+            context = {'form': ProjectForm(initial=project)}
+            return render(request, 'ids_projects/create.html', context)
 
     ########
     # POST #
     ########
     elif request.method == 'POST':
-        return HttpResponse("Creating new project: {}".format(len(projects)+1))
+
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+
+            user_full = request.user.get_full_name()
+
+            title = form.cleaned_data['title']
+            inv_type = form.cleaned_data['investigation_type']
+            desc = form.cleaned_data['description']
+
+            new_project = {
+                'name' : 'idsvc.project',
+                'associationIds' : [],
+                'value' : {
+                    'title' : title,
+                    'creator' : user_full,
+                    'investigation_type' : inv_type,
+                    'description' : desc,
+                },
+            }
+
+            a = _client(request)
+            try:
+                response = a.meta.updateMetadata(uuid=project_id, body=new_project)
+            except Exception as e:
+                logger.exception('Error while attempting to edit project metadata.')
+
+        return HttpResponseRedirect('/projects/')
 
     #########
     # OTHER #
@@ -140,7 +195,7 @@ def delete(request, project_id):
         query = {'uuid':project_id}
         project_list = a.meta.listMetadata(q=json.dumps(query))
 
-        print project_list
+        print '\n\n *** delete : {} *** \n\n'.format(project_list)
 
         try:
             project = project_list[0]
