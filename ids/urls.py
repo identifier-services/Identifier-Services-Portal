8445ec22b037bbcbe1d0454438e7d0794aa9c411
Andrew Magill <andrewbmagill@yahoo.com>
2016-04-06 14:31:28 -0500

    removing commented out routes

--- ids/urls.py
+++ ids/urls.py
@@ -12,21 +12,6 @@ urlpatterns = patterns(
     # project-level urls
     url(r'^$', 'ids.views.homepage', name='homepage'),
 
-    # projects
-    # url(r'^projects/', include('ids_projects.urls', namespace='ids_projects')),
-    #
-    # # specimens
-    # url(r'^specimens/', include('ids_specimens.urls', namespace='ids_specimens')),
-    #
-    # # datasets
-    # url(r'^datasets/', include('ids_datasets.urls', namespace='ids_datasets')),
-    #
-    # # datasets
-    # url(r'^data/', include('ids_data.urls', namespace='ids_data')),
-    #
-    # # systems
-    # url(r'^systems/', include('ids_systems.urls', namespace='ids_systems')),
-
     # auth
     url(r'^auth/', include('ids_auth.urls', namespace='ids_auth')),
     url(r'^login/$', 'ids_auth.views.agave_oauth', name='login'),
