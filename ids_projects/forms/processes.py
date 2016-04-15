858cf57d2b01212b0926c96a119e0b3877648a89
Andrew Magill <andrewbmagill@yahoo.com>
2016-04-15 15:20:15 -0500

    starting to work on models - this should be mrhanlon's commit

--- ids_projects/forms/processes.py
+++ ids_projects/forms/processes.py
@@ -1,27 +1,9 @@
 import logging
 from django import forms
-
+from .base import DynamicForm
+import six
 
 logger = logging.getLogger(__name__)
-
-
-def _construct_form_field(f):
-    field_class_name    = f.get('field_class', 'CharField')
-    field_id            = f.get('id', None)
-    field_label         = f.get('label', field_id)
-    widget              = f.get('widget', 'TextInput')
-    choices             = f.get('choices', None)
-    required            = f.get('required', None)
-    field_class         = getattr(forms, field_class_name)
-    if choices:
-        choices_tuple = tuple([(x,x) for x in choices])
-        return forms.ChoiceField(label=field_label,
-                                 choices=choices_tuple,
-                                 required=required)
-    else:
-        return field_class(label=field_label,
-                           widget=getattr(forms, widget),
-                           required=required)
 
 
 class ProcessTypeForm(forms.Form):
@@ -31,10 +13,73 @@ class ProcessTypeForm(forms.Form):
         super(ProcessTypeForm, self).__init__(*args, **kwargs)
         self.fields['process_type'].choices = choices
 
+class IdsMetadata(object):
 
-class ProcessFieldsForm(forms.Form):
+    def __init__(self, uuid=None, *args, **kwargs):
+        self.uuid = uuid
+        self.value = {}
+        self.created = None
 
-    def __init__(self, fields, *args, **kwargs):
-        super(ProcessFieldsForm, self).__init__(*args, **kwargs)
-        for f in fields:
-            self.fields[f['id']] = _construct_form_field(f)
+        if self.uuid:
+            ag = get_agave_client()
+            meta = ag.meta.getMetadata(uuid=uuid)
+            self.value = meta.value
+
+    def get_associated(self, name=None, **kwargs):
+        query = {
+            'associationIds': [self.uuid]
+        }
+        if name:
+            query['name'] = name
+
+        for k, v in six.iteritems(kwargs)
+            key = 'value.%s' % k
+            query[key] = v
+
+        ag = get_agave_client()
+        return ag.meta.listMetadata(q=json.dumps(query))
+
+
+class Process(IdsMetadata):
+    name = 'idsvc.process'
+
+    @property
+    def data(self):
+        if not self._data:
+            self._data = self.get_associated(name='idsvc.data')
+
+        return self._data
+
+    @property
+    def inputs(self):
+        if not self._inputs:
+            self._inputs = [d for d in self.data
+                            if d.uuid in self.value.input_uuids]
+
+        return self._inputs
+
+    @property
+    def outputs(self):
+        if not self._outputs:
+            self._outputs = [d for d in self.data
+                            if d.uuid in self.value.output_uuids]
+
+        return self._outputs
+
+    def __init__(self, uuid=None, *args, **kwargs):
+        super(Process, self).__init__(uuid, *args, **kwargs)
+
+class ProcessFieldsForm(DynamicForm):
+
+    metadata_model = Process
+
+# p = Process(uuid='asdfasdfasdf')
+#
+# inputs = p.inputs
+#
+# 
+# p.get_associated(name='idsvc.data', type='sra_data')
+# query = {
+#     'name': 'idsvc.data',
+#     'value.type': 'sra_data'
+# }
