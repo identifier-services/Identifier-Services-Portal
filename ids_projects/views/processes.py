59c9e5a3b1d5abe6543e05f0439334954edf9eee
Andrew Magill <andrewbmagill@yahoo.com>
2016-04-29 13:40:39 -0500

    using model in process create view

--- ids_projects/views/processes.py
+++ ids_projects/views/processes.py
@@ -125,7 +125,7 @@ def create(request):
             if form_a.is_valid() and form_b.is_valid():
                 logger.debug('Process form is valid')
 
-                data = {}
+                data = {'process_type':process_type}
                 data.update(form_a.cleaned_data.copy())
                 data.update(form_b.cleaned_data.copy())
 
@@ -155,6 +155,60 @@ def create(request):
 
 @login_required
 def edit(request, process_uuid):
+    """Edit existing process metadata"""
+    try:
+        process = Process(uuid=process_uuid)
+    except HTTPError as e:
+        logger.error('Error editing process. {}'.format(e.message))
+        messages.error(request, 'Error editing process.')
+        return HttpResponseRedirect('/process/{}'.format(process_uuid))
+    except Exception as e:
+        logger.error('Error editing process. {}'.format(e.message))
+        messages.error(request, 'Process not found.')
+        return HttpResponseRedirect('/projects/')
+
+    # TODO: find better way of getting process fields.
+    object_descriptions = getattr(settings, 'OBJ_DESCR')
+    investigation_types = object_descriptions['investigation_types']
+    investigation_type = process.project.value['investigation_type'].lower()
+
+    project_description = investigation_types[investigation_type]
+    project_processes = project_description['processes']
+    process_type = process.value['process_type']
+    process_fields = project_processes[process_type]['fields']
+
+    #######
+    # GET #
+    #######
+    if request.method == 'GET':
+        context = {'form': ProcessFieldsForm(process_fields, initial=process.body),
+                   'specimen': process.specimen,
+                   'project': process.project,
+                   'process': process}
+
+        return render(request, 'ids_projects/processes/edit.html', context)
+
+    ########
+    # POST #
+    ########
+    elif request.method == 'POST':
+        form = ProcessFieldsForm(process_fields, request.POST)
+
+        if form.is_valid():
+            try:
+                process.value = form.cleaned_data.copy()
+                process.save()
+                logger.error('Successfully edited process.')
+                messages.error(request, 'Successfully edited process.')
+                return HttpResponseRedirect('/process/{}'.format(process_uuid))
+            except HTTPError as e:
+                logger.error('Error editing process. {}'.format(e.message))
+                messages.error(request, 'Error editing process.')
+                return HttpResponseRedirect('/process/{}'.format(process_uuid))
+
+
+@login_required
+def _edit(request, process_uuid):
     """ """
     #######
     # GET #
@@ -262,55 +316,26 @@ def edit(request, process_uuid):
 @login_required
 def delete(request, process_uuid):
     """Delete a process"""
-    #######
-    # GET #
-    #######
-    if request.method == 'GET':
+    process = Process(uuid=process_uuid)
 
-        # TODO: Ask user if file data should be deleted
-
-        # get the process
-        a = client(request)
-        process_raw = a.meta.getMetadata(uuid=process_uuid)
-        process = collapse_meta(process_raw)
-
-        project = None
-        specimen = None
-
-        # find project & specimen the process is associated with
-        associationIds = process['associationIds']
-        query = {'uuid': { '$in': associationIds }}
-        results_raw = a.meta.listMetadata(q=json.dumps(query))
-        results = map(collapse_meta, results_raw)
-        for result in results:
-            if result['name'] == 'idsvc.specimen':
-                specimen = result
-            if result['name'] == 'idsvc.project':
-                project = result
-
+    # TODO: delete data, or just associate data with project?
+    for data in process.data:
+        # remove process_uuid from associationIds on data, leaving only project_uuid
         try:
-            a.meta.deleteMetadata(uuid=process_uuid)
-        except Exception as e:
-            logger.error('Error deleting process. {}'.format(e.message) )
-            messages.error(request, 'Process deletion unsuccessful.')
-            # return HttpResponseServerError("Error deleting project.")
-            if specimen:
-                return HttpResponseRedirect('/specimen/{}'.format(specimen['uuid']))
-            if project:
-                return HttpResponseRedirect('/project/{}'.format(project['uuid']))
-            else:
-                return HttpResponseRedirect('/projects/')
-        else:
-            messages.success(request, 'Successfully deleted process.')
-            if specimen:
-                return HttpResponseRedirect('/specimen/{}'.format(specimen['uuid']))
-            if project:
-                return HttpResponseRedirect('/project/{}'.format(project['uuid']))
-            else:
-                return HttpResponseRedirect('/projects/')
+            data.associationIds.remove(process_uuid)
+        except HTTPError as e:
+            logger.debug('Error while attempting to remove process_uuid from data metadata: %s' % e)
 
-    #########
-    # OTHER #
-    #########
+    try:
+        project = process.project
+        specimen = process.specimen
+        process.delete()
+        messages.success(request, 'Successfully deleted process.')
+    except HTTPError as e:
+        logger.debug('Error while attempting to delete process metadata: %s' % e)
+        messages.success(request, 'Encountered error while attempting to delete process.')
+
+    if specimen:
+        return HttpResponseRedirect('/specimen/{}'.format(specimen.uuid))
     else:
-        django.http.HttpResponseNotAllowed("Method not allowed")
+        return HttpResponseRedirect('/project/{}'.format(project.uuid))
