b0bc91f137439402ed7d2c9e415bf41e50a7562d
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-05-09 16:18:28 -0500

    fix static files

--- ids/settings.py
+++ ids/settings.py
@@ -115,12 +115,14 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'ids', 'static')
-
+STATIC_ROOT = '/opt/ids/static/'
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = '/opt/ids/media/'
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = (
-    
+    os.path.join(BASE_DIR, 'ids', 'static'),
 )
 
 LOGIN_URL = '/login/'
