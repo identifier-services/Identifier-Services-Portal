afb8d1043a9250b564de5897cffc5dcc41e915ae
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-06-07 15:56:34 -0500

    add new token methods

---
+++ ids/utils.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from agavepy.agave import Agave
+
+
+def get_portal_api_client():
+    return Agave(api_server=settings.AGAVE_TENANT_BASEURL,
+                 toke=settings.AGAVE_SUPER_TOKEN)
