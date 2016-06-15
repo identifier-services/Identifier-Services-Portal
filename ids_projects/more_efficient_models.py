74046ec0b72c8127a772a5f412240983f85ea139
Andrew Magill <andrewbmagill@yahoo.com>
2016-06-15 18:15:13 -0500

    new model tests

--- ids_projects/more_efficient_models.py
+++ ids_projects/more_efficient_models.py
@@ -5,7 +5,7 @@ class BaseAgaveObject(object):
     client at the moment."""
     def __init__(self, api_client, *args, **kwargs):
         # TODO: if type(api_client) is not agavepy.agave.Agave: raise Exception()
-        self.api_client = api_client
+        self._api_client = api_client
 
 class BaseMetadata(BaseAgaveObject):
     """Base class for IDS Metadata (Project, Specimen, Process, Data)"""
@@ -23,12 +23,49 @@ class BaseMetadata(BaseAgaveObject):
         """
         super(BaseMetadata, self).__init__(*args, **kwargs)
 
+        # get meta if passed to constructor, convert to dict if necessary
+
         meta = kwargs.get('meta', {})
+        if type(meta) is str:
+            meta = json.loads(meta)
         self.load_from_meta(meta)
 
-        # explicit parameters take precedence over values found in meta dictionary
+        # explicit constructor parameters take precedence over values found in
+        # meta dictionary (namely 'uuid', and 'value' aka 'body')
+
+        body = kwargs.get('body', meta.get('value', None))
+        if type(body) is str:
+            body = json.loads(body)
+        self.body = body
         self.uuid = kwargs.get('uuid', meta.get('uuid', None))
-        self.body = kwargs.get('body', meta.get('value', None))
+
+        # create these instance variables for later
+
+        self._upstream_objects = None
+        self._downstream_objects = None
+
+    def flush_associated(self):
+        """Clears cached associated objects"""
+        self._upstream_objects = None
+        self._downstream_objects = None
+
+    @property
+    def upstream_objects(self):
+        """Retrieves all metadata objects corresponding to this instance's associationIds"""
+        if self._upstream_objects is None:
+            query = { 'uuid': { '$in': self.associationIds } }
+            self._upstream_objects = self._api_client.meta.listMetadata(q=json.dumps(query))
+
+        return self._upstream_objects
+
+    @property
+    def downstream_objects(self):
+        """Retrieves all metadata objects that have this instance's UUID in their associationIds"""
+        if self._downstream_objects is None:
+            query = { 'associationIds': self.uuid }
+            self._downstream_objects = self._api_client.meta.listMetadata(q=json.dumps(query))
+
+        return self._downstream_objects
 
     def load_from_meta(self, meta):
         """Set instance variables from dictionary or json"""
@@ -53,20 +90,32 @@ class BaseMetadata(BaseAgaveObject):
 
     def save(self):
         """Add or update metadata object on tenant"""
+
         if self.uuid is None:
-            response = self.api_client.meta.addMetadata(body=self.meta)
+            response = self._api_client.meta.addMetadata(body=self.meta)
         else:
-            response = self.api_client.meta.updateMetadata(uuid=self.uuid, body=self.meta)
+            response = self._api_client.meta.updateMetadata(uuid=self.uuid, body=self.meta)
         self.load_from_meta(response)
+
+        return response
 
     def delete(self):
         """Delete metadata object on tenant"""
-        if self.uuid is not None:
-            self.api_client.meta.deleteMetadata(uuid=self.uuid)
-            self.uuid = None
+        if self.uuid is None:
+            raise Exception('Cannot delete without UUID.')
+
+        response = self._api_client.meta.deleteMetadata(uuid=self.uuid)
+        self.uuid = None
+        return response
 
     @property
     def meta(self):
+        """Dictionary containing all relevant data and metadata. Fields may include:
+
+            uuid, owner, schemaId, internalUsername, associationIds, lastUpdated,
+            name, created, value (may contain an embedded dictionary), _links
+        """
+        # TODO: I'm not sure if it makes sense to strip out null values
         return { 'uuid': self.uuid,
                  'owner': self.owner,
                  'schemaId': self.schemaId,
@@ -79,6 +128,7 @@ class BaseMetadata(BaseAgaveObject):
                  '_links': self._links }
 
     def to_json(self):
+        """The meta property, formatted as json string"""
         return json.dumps(self.meta)
 
 class Project(BaseMetadata):
@@ -115,13 +165,37 @@ class Data(BaseMetadata):
 
 # questions:
 #   * create all objects with user client (particular client determined in the view)?
-#   * share with system agave user to make public?
+#   * share with system agave user to make public? - maybe share with ids user to begin with
 #       - remember, all project objects need to be shared, including future objects after a project is made public
-#   * no public flag?
+#   * webhook? not initiated by user... that's fine, we're going to update checksum as system client
+#       - the bug we found will need to be fixed... and it is
+#   * no public flag? - if we share everything with the system user, we will need a public flag
+#       - maybe silly question but is there any sense in doing this: name=idsv.project.public? No, that's silly
+#       - published better than public
+#       - additional flags at project level:
+#           '_published' t/f
+#           '_has_unpublished' t/f
+#           don't automatically publish new stuff
+#           show new stuff that could be published project detail view
 #   * scenerio, we have a new view for public projects (we also have a view for private projects)
 #     the user clicks 'view' on a public project (that the user doesn't own). the app routes the user to /project/uuid
 #       - how does the project-detail view know that the project is a public project not owned by the user?
-#       - should we have separate views for public projects? /project/public/uuid?
-#   * how do we list Specimens, Processes, Data, if there is no heirarchy (other than everything a chile of Project)?
+#           try user client first? if not, try system client? see table below
+#
+#       - should we have separate views for public projects? /project/public/uuid? No, don't like it
+#   * how do we list Specimens, Processes, Data, if there is no heirarchy (other than everything a child of Project)?
 #       - is there actually a 'dynamic' heirarchy? dependent on the type of project?
 #       - how would that work?
+
+# need to go to view to edit
+# put delete button in the edit view
+
+#           which client to use?
+#                   portal  |   user
+# list public           X   |
+# my projects               |   X
+# view any models       X   |   ?        // read first with portal, fallback to user
+# add/update                |   X
+# webhook callback      X   |            // only write that we do as the portal client
+# list systems              |   X
+# list files                |   X
--- ids_projects/tests.py
+++ ids_projects/tests.py
@@ -1,3 +1,157 @@
 from django.test import TestCase
+from agavepy.agave import Agave
+from more_efficient_models import (BaseAgaveObject, BaseMetadata, Project,
+                                   Specimen, Process, Data)
+import os
 
-# Create your tests here.
+# import subprocess
+# base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# test_path = os.path.join(base_dir,'test.env')
+# subprocess.call(['/bin/bash',test_path])
+
+# instantiating these clients outside of the test suite setup method might be
+# frowned upon, but I only want to create the clients once. it's time consuming.
+
+
+AGAVE_TENANT_BASEURL = os.environ.get('AGAVE_TENANT_BASEURL')
+IDS_SYS_SUPER_TOKEN = os.environ.get('IDS_SYS_SUPER_TOKEN')
+TEST_USER1_SUPER_TOKEN = os.environ.get('TEST_USER1_SUPER_TOKEN')
+TEST_USER2_SUPER_TOKEN = os.environ.get('TEST_USER2_SUPER_TOKEN')
+
+IDS_SYS_CLEINT = Agave(api_server=AGAVE_TENANT_BASEURL, token=IDS_SYS_SUPER_TOKEN)
+TEST_USER1_CLIENT = Agave(api_server=AGAVE_TENANT_BASEURL, token=TEST_USER1_SUPER_TOKEN)
+TEST_USER2_CLIENT = Agave(api_server=AGAVE_TENANT_BASEURL, token=TEST_USER2_SUPER_TOKEN)
+
+
+class BaseClientTests(object):
+    """Test class is inteded to be inherited"""
+
+    # make sure we got the env variables
+
+    def test_environ_tenant_baseurl(self):
+        """Did we get the baseurl env variable?"""
+        self.assertIsNotNone(AGAVE_TENANT_BASEURL)
+
+    def test_environ_sys_token(self):
+        """Did we get the ids sys super token env variable?"""
+        self.assertIsNotNone(IDS_SYS_SUPER_TOKEN)
+
+    def test_environ_testuser1_token(self):
+        """Did we get the test user 1 env variable?"""
+        self.assertIsNotNone(TEST_USER1_SUPER_TOKEN)
+
+    def test_environ_testuser2_token(self):
+        """Did we get the test user 2 env variable?"""
+        self.assertIsNotNone(TEST_USER2_SUPER_TOKEN)
+
+    # make sure we can use out clients
+
+    def test_sys_client(self):
+        """Test ids sys client, should return dict with username for profiles.listByUsername"""
+        self.assertIsNotNone(IDS_SYS_CLEINT.profiles.listByUsername(username='me'))
+
+    def test_testuser1_client(self):
+        """Test test user 1 client, should return dict with username for profiles.listByUsername"""
+        self.assertIsNotNone(TEST_USER1_CLIENT.profiles.listByUsername(username='me'))
+
+    def test_testuser2_client(self):
+        """Test test user 2 client, should return dict with username for profiles.listByUsername"""
+        self.assertIsNotNone(TEST_USER2_CLIENT.profiles.listByUsername(username='me'))
+
+
+class BaseMetadataTests(TestCase, BaseClientTests):
+    """Tests for BaseAgaveObject and BaseMetadata in IDS models"""
+
+    def test_base_agave_object(self):
+        """Test instantiation of base object"""
+        base_object = BaseAgaveObject(api_client=IDS_SYS_CLEINT)
+        self.assertIsNotNone(base_object)
+        self.assertEqual(base_object._api_client, IDS_SYS_CLEINT)
+
+    def test_base_metadata(self):
+        """Test instantiation of base metadata object"""
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT)
+        self.assertIsNotNone(base_meta)
+        self.assertEqual(base_meta._api_client, IDS_SYS_CLEINT)
+
+    def test_uuid_base_metadata_param(self):
+        """Test uuid parameter to base metadata constructor"""
+        uuid = 'ABC'
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, uuid=uuid)
+        self.assertEqual(base_meta.uuid, uuid)
+
+    def test_body_base_metadata_param(self):
+        """Test body parameter to base metadata constructor"""
+        body = { 'color': 'blue' }
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, body=body)
+        self.assertEqual(base_meta.body, body)
+
+    def test_meta_base_metadata_param(self):
+        """Test meta parameter to base metadata constructor"""
+        name = 'idsvc-test-meta'
+        meta = { 'name': name }
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, meta=meta)
+        # base_meta.meta will return null values that we did not specifiy
+        # so we need to test if meta is a subset of base_meta.meta
+        self.assertTrue(all(item in base_meta.meta.items() for item in meta.items()))
+
+    def test_name_attribute_in_base_metadata(self):
+        """Test name attribute in base metadata object"""
+        name = 'idsvc-test-meta'
+        meta = { 'name': name }
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, meta=meta)
+        self.assertEqual(base_meta.name, meta['name'])
+
+    def test_all_attributes_in_base_metadata(self):
+        """Test name attribute in base metadata object"""
+
+        name = 'idsvc-test-meta'
+        uuid = 'ABC'
+        body = { 'color': 'blue' }
+        owner = 'bob'
+        schemaId = None
+        internalUsername = None
+        associationIds = ['CDE', 'EFG']
+        lastUpdated = '2016-06-15T17:09:06.137-05:00'
+        name = 'idsvc-test-meta'
+        created = '2016-06-15T17:09:06.137-05:00'
+        _links =  ''
+
+        meta = {
+            'name': name,
+            'uuid': uuid,
+            'value': body,
+            'owner': owner,
+            'schemaId': schemaId,
+            'internalUsername': internalUsername,
+            'associationIds': associationIds,
+            'lastUpdated': lastUpdated,
+            'name': name,
+            'created': created,
+            '_links': _links
+         }
+
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, meta=meta)
+        self.assertDictEqual(base_meta.meta, meta)
+
+    def test_save_base_metadata(self):
+        """Test saving a base metadata object"""
+
+        name = 'idsvc-test-meta'
+        body = { 'color': 'blue' }
+        name = 'idsvc-test-meta'
+
+        meta = { 'name': name,
+                 'value': body,
+                 'name': name }
+
+        base_meta = BaseMetadata(api_client=IDS_SYS_CLEINT, meta=meta)
+        response = base_meta.save()
+        self.assertIn('uuid', response)
+        self.assertIsNotNone(base_meta.uuid)
+
+        self.assertTrue(all(item in base_meta.meta.items() for item in meta.items()))
+
+    # def test_save_delete_meta(self):
+    #     """Test save and delete, I don't know the best way to do this..."""
+    #     self.assertIsEqual(1,2)
