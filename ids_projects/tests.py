a31c0660bdbce6f277f9c9a60dfef1c18e22cbfa
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-07-18 17:23:03 -0500

    start some view tests

--- ids_projects/tests.py
+++ ids_projects/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
 from agavepy.agave import Agave
 from models import (BaseAgaveObject, BaseMetadata, Project,
                                    Specimen, Process, Data, System)
-import os, logging
+import os
+import logging
+import mock
 
 logger = logging.getLogger(__name__)
 
@@ -535,3 +539,27 @@ class SystemTests(TestCase, BaseClientTests):
         # pprint.pprint(listing)
 
         # self.assertTrue(any([sys.id == 'lonestar5.tacc.utexas.edu' for sys in system_list]))
+
+
+class ViewTests(TestCase):
+
+    fixtures = ['user_data.json']
+
+    def test_list_datasets_view(self):
+        response = self.client.get(reverse('ids_projects:dataset-list-public'))
+        self.assertContains(response, '<h1>Public Datasets</h1>')
+
+    def test_list_user_projects_anon(self):
+        response = self.client.get(reverse('ids_projects:project-list-private'))
+        self.assertRedirects(response, '/login/?next=/projects/private', status_code=302,
+                             fetch_redirect_response=False)
+
+    @mock.patch('ids_projects.models.Project.list')
+    def test_list_user_projects(self, mock_project_list):
+        self.client.login(username='ids_user_1', password='testing')
+        response = self.client.get(reverse('ids_projects:project-list-private'))
+
+        assert mock_project_list.called
+        self.assertContains(response, "<h3>User's Projects</h3>")
+
+
